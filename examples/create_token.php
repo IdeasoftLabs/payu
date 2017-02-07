18d9c071acb9aabe0a04376652ff026e94c02ad4
Murat SAC <murat.sac@ideasoft.com.tr>
2017-02-07 14:28:04 +0300

    namespace updated

--- examples/create_token.php
+++ examples/create_token.php
@@ -1,14 +1,21 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$creditCard = new \PayU\Parameter\Model\CreditCard();
+use IdeasoftLabs\PayU\Parameter\Model\CreditCard;
+use IdeasoftLabs\PayU\Parameter\Model\BillingAddress;
+use IdeasoftLabs\PayU\Parameter\Model\DeliveryAddress;
+use IdeasoftLabs\PayU\Parameter\Model\OrderItem;
+use IdeasoftLabs\PayU\Parameter\CreateTokenParam;
+use IdeasoftLabs\PayU\Request\CreateTokenRequest;
+
+$creditCard = new CreditCard();
 $creditCard->setHolderName('IdeaSoft labs');
 $creditCard->setNumber('4355084355084358');
 $creditCard->setExpiryMonth('12');
 $creditCard->setExpiryYear('2018');
 $creditCard->setCvv('000');
 
-$billingAddress = new \PayU\Parameter\Model\BillingAddress();
+$billingAddress = new BillingAddress();
 $billingAddress->setFirstName('Murat');
 $billingAddress->setLastName('Saç');
 $billingAddress->setEmail('murat.sac@ideasoft.com.tr');
@@ -21,7 +28,7 @@ $billingAddress->setCountryCode('TR');
 $billingAddress->setState('State');
 $billingAddress->setFax('1234567890');
 
-$deliveryAddress = new \PayU\Parameter\Model\DeliveryAddress();
+$deliveryAddress = new DeliveryAddress();
 $deliveryAddress->setFirstName('Murat');
 $deliveryAddress->setLastName('Saç');
 $deliveryAddress->setEmail('murat.sac@ideasoft.com.tr');
@@ -34,7 +41,7 @@ $deliveryAddress->setCountryCode('TR');
 $deliveryAddress->setState('State');
 $deliveryAddress->setFax('1234567890');
 
-$orderItem = new \PayU\Parameter\Model\OrderItem();
+$orderItem = new OrderItem();
 $orderItem->setName('Ticket1');
 $orderItem->setCode('TCK1');
 $orderItem->setInfo('Barcelona flight');
@@ -42,12 +49,12 @@ $orderItem->setPrice('100');
 $orderItem->setVat('qw2');
 $orderItem->setQuantity('1');
 
-$param = new \PayU\Parameter\CreateTokenParam();
+$param = new CreateTokenParam();
 $param->setPostUrl('https://secure.payu.com.tr/order/alu/v3');
 $param->setSecretKey('SECRET_KEY');
 $param->setMerchant('OPU_TEST');
 $param->setIpAddress('127.0.0.1');
-$param->setOrderRef('112323245');
+$param->setOrderRef('988012');
 $param->setOrderDate(gmdate('Y-m-d H:i:s'));
 $param->setCurrency('TRY');
 $param->setInstallment('1');
@@ -57,6 +64,7 @@ $param->setDeliveryAddress($deliveryAddress);
 $param->addOrderItem($orderItem);
 
 // request
-$request = new \PayU\Request\CreateTokenRequest($param);
+$request = new CreateTokenRequest($param);
 $response = $request->send();
+var_dump($response);
 
